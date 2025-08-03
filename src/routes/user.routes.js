8ad56d2b0a3265b9ff92d6a74e2cad3874897db8
bamlak21 <bamlakgezahegn27@gmail.com>
2025-08-03 18:07:26 +0300

    Docs

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -101,7 +101,143 @@ router.get("/profile/:id", getCurrentUserProfile);
 
 router.patch("/update", UpdateUserProfile);
 
+/**
+ * @swagger
+ * /api/users/getall:
+ *   get:
+ *     summary: Fetch all users
+ *     tags:
+ *       - User
+ *     responses:
+ *       200:
+ *         description: A list of all users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   _id:
+ *                     type: string
+ *                     example: 64f1b9e7d8f5a123456789ab
+ *                   firstName:
+ *                     type: string
+ *                     example: john
+ *                   lastName:
+ *                     type: string
+ *                     example: doe
+ *                   email:
+ *                     type: string
+ *                     example: john.doe@example.com
+ *                   phoneNumber:
+ *                     type: string
+ *                     example: +251911223344
+ *                   profileImage:
+ *                     type: string
+ *                     example: uploads/profile/john.jpg
+ *                   socialLinks:
+ *                     type: object
+ *                     properties:
+ *                       facebook:
+ *                         type: string
+ *                         example: https://facebook.com/johndoe
+ *                       twitter:
+ *                         type: string
+ *                         example: https://twitter.com/johndoe
+ *       500:
+ *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Internal Server Error
+ */
+
 router.get("/getall", fetchAllUsers);
+
+/**
+ * @swagger
+ * /api/users/deactivate/{id}:
+ *   patch:
+ *     summary: Deactivate or activate a user
+ *     tags:
+ *       - User
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the user to update
+ *         example: 64f1b9e7d8f5a123456789ab
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - isActive
+ *             properties:
+ *               isActive:
+ *                 type: boolean
+ *                 description: User activation status
+ *                 example: false
+ *     responses:
+ *       200:
+ *         description: User activation status updated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: User deactivated successfully
+ *                 user:
+ *                   type: object
+ *                   description: The updated user object
+ *                   properties:
+ *                     _id:
+ *                       type: string
+ *                       example: 64f1b9e7d8f5a123456789ab
+ *                     firstName:
+ *                       type: string
+ *                       example: john
+ *                     lastName:
+ *                       type: string
+ *                       example: doe
+ *                     email:
+ *                       type: string
+ *                       example: john.doe@example.com
+ *                     isActive:
+ *                       type: boolean
+ *                       example: false
+ *       404:
+ *         description: User not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: User not found
+ *       500:
+ *         description: Server error while deactivating user
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Server error while deactivating user
+ */
 
 router.patch("/deactivate/:id", deactivateUser);
 
