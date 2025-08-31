1e81ab20e003c6ab3dfd39a763576358e4f44dcc
Robel <robel.getaneh27@gmail.com>
2025-08-31 17:11:28 +0300

    fixes

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -269,6 +269,11 @@ export const verifyUser = async (req, res) => {
     if (!updatedUser) {
       return res.status(404).json({ message: "User not found" });
     }
+    await CreateNotification({
+      userId: updatedUser._id,
+      type: "approved",
+      message: "Your account verified successfully.",
+    });
 
     return res.status(200).json(updatedUser);
   } catch (error) {
